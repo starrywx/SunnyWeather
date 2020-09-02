51ea58f0caac30df6db5c6ed724f1e1b438bc95a
weixiao <1163401806@qq.com>
2020-09-02 15:40:03 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/example/sunnyweather/logic/network/WeatherService.kt
@@ -0,0 +1,8 @@
+package com.example.sunnyweather.logic.network
+
+import com.example.sunnyweather.SunnyWeatherApplication
+import retrofit2.http.GET
+
+interface WeatherService {
+    //@GET("v2.5/${SunnyWeatherApplication.TOKEN}/{lng},{lat}realtime.json")
+}
